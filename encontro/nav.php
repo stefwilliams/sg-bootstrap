6607225fec37ea866576a1a78bb2e3e22faad3bf
stefwilliams <stefwilliams@gmail.com>
2014-12-27 13:10:17 +0000

    made encontro logo a link to encontro home

--- encontro/nav.php
+++ encontro/nav.php
@@ -1,27 +1,54 @@
 <section id="nav" class="encontro-logo span3">
+
 	<div class="encontro-logo">
+		<a href="/encontro2015"><img src="/wp-content/themes/sg-bootstrap/encontro/encontro.png" alt="Encontro 2015 Home" /></a>
+
 	</div>
+
 	<?php
+
 global $post;
 
+
+
 	$encontro_home_slug = "encontro2015";
+
 	$encontro_home_object = get_page_by_path( $encontro_home_slug);
+
 	$encontro_home_id = $encontro_home_object->ID;
 
+
+
 		$args = array(
+
 			'child_of'     => $encontro_home_id,
+
 			'link_after'   => '',
+
 			'link_before'  => '',
+
 			'post_type'    => 'page',
+
 			'post_status'  => 'publish',
+
 			'sort_column'  => 'menu_order',
+
 			'title_li'     => ''
+
 		);
+
 echo "<ul class='encontro nav nav-tabs nav-stacked'>";
+
 			wp_list_pages( $args );
+
 echo "</ul>";			
+
 		?>
+
 	
+
 <!-- print_r($wp_query); ?> -->
+
+
 
 </section>
